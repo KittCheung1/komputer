063a5cb1c1ecd005d53bd95d1233635b677f0a4f
Kitt Cheung <kitt.cheung@live.se>
2022-01-15 17:07:18 +0100

    added "repay loan" button and events for it

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const btnWork = document.getElementById("work");
 const btnBank = document.getElementById("bank");
 const btnLoan = document.getElementById("loan");
+const btnRepay = document.getElementById("repay");
 // const btnBuy =  document.getElementById("buy");
 
 const sumOfLoan = document.getElementById("sum-of-loan");
@@ -28,32 +29,31 @@ function banking() {
         balance.innerText = currentBalance;
     }
     if (currentBalance != 0 && parseInt(sumOfLoan.innerText) != 0){
-        let deductedMoney = parseInt(earnedMoney.innerText * 0.9);
+        let deductedMoney = parseFloat(earnedMoney.innerText * 0.9);
         balance.innerText = currentBalance + deductedMoney;
-        // sumOfLoan.innerText = sumOfLoan.innerText - parseInt(earnedMoney.innerText) * 0.1;
+        sumOfLoan.innerText = parseInt(sumOfLoan.innerText) + parseFloat(earnedMoney.innerText) * 0.1;
     }
     if (sumOfLoan.innerText == 0) {
         document.getElementById("current-loan").hidden = true;
-        document.getElementById("sum-of-loan").hidden = true;
+        sumOfLoan.hidden = true;
+        btnRepay.hidden = true;
     }
     earnedMoney.innerText = 0;
 }
 
 function loaning() {
     if (parseInt(moneyInBank.innerText) !=0 && parseInt(sumOfLoan.innerText) == 0) {
-        const loanAmount = Number(window.prompt("How much do you want to loan?"));
-        if (loanAmount == 0 || loanAmount !== ""){
-            alert("input error");
-        }
-        else if (loanAmount <= parseInt(balance.innerText) * 2) {
+        const loanAmount = parseInt(prompt("How much do you want to loan?"));
+        
+        if (loanAmount != 0 && loanAmount <= parseInt(balance.innerText) * 2){
             alert("loan accepted!");
             document.getElementById("current-loan").hidden = false;
-            document.getElementById("sum-of-loan").hidden = false;
+            sumOfLoan.hidden = false;
             sumOfLoan.innerText = loanAmount;
+            btnRepay.hidden = false;
         }
         else {
-            alert("loan NOT accepted!");
-            sumOfLoan.innerText = 0;
+            alert("input error, loan NOT accepted!");
         }
     }
     else {
@@ -61,10 +61,22 @@ function loaning() {
     }
 }
 
-
+function toRepay(){
+    if(parseInt(moneyInBank.innerText) >= parseInt(sumOfLoan.innerText)){
+        let deductLoan = parseInt(moneyInBank.innerText) - parseInt(sumOfLoan.innerText);
+        moneyInBank.innerText = deductLoan;
+        sumOfLoan.innerText = 0;
+        console.log(deductLoan);
+        document.getElementById("current-loan").hidden = true;
+        sumOfLoan.hidden = true;
+        btnRepay.hidden = true;
+    }
+    else alert("not enough balance");
+}
 
 
 
 btnWork.addEventListener("click", increment);
 btnBank.addEventListener("click", banking);
 btnLoan.addEventListener("click", loaning);
+btnRepay.addEventListener("click", toRepay);
